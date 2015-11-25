2b963e6b9e37cfaf21f7d102f8e8e5fc480973c8
Geoff Pitfield <geoff@pitfield.com>
2015-11-24 20:49:41 -0800

    handle null values properly

--- valuesMapper.go
+++ valuesMapper.go
@@ -8,14 +8,21 @@ import (
 func ValuesMapper(values []string, item interface{}) (out map[string]string) {
 	itemValue := reflect.ValueOf(item)
 	out = make(map[string]string)
+	var fieldInterface interface{}
+	var val reflect.Value
 	for _, field := range values {
-		fieldInterface := reflect.Indirect(itemValue.FieldByName(field)).Interface()
-		v, ok := fieldInterface.(fmt.Stringer)
-		if ok {
-			out[field] = v.String()
+		val = reflect.Indirect(itemValue.FieldByName(field))
+		if val.IsValid() {
+			fieldInterface = val.Interface()
+			v, ok := fieldInterface.(fmt.Stringer)
+			if ok {
+				out[field] = v.String()
+			} else {
+				// out[field] = fieldInterface
+				out[field] = fmt.Sprintf("%v", fieldInterface)
+			}
 		} else {
-			// out[field] = fieldInterface
-			out[field] = fmt.Sprintf("%v", fieldInterface)
+			out[field] = ""
 		}
 	}
 	return out
